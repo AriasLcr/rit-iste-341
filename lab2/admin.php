44295fc47d083953004fa790e1c8978422ecf675
AriasLcr <ariaslcr@gmail.com>
2026-09-07 12:35:21 -0400

    lab2 complete

--- lab2/admin.php
+++ lab2/admin.php
@@ -2,16 +2,17 @@
     session_name('login');
     session_start();
 
+    // redirect to login with error message "You need to log in"
     if (!isset($_SESSION['loggedIn']) || $_SESSION['loggedIn'] !== true) {
         header('Location: login.php?msg=needlogin');
         exit;
     }
 
-    $loginTime = isset($_COOKIE['loggedIn']);
-
+    // Set cookie to immediately expire
     setcookie('loggedIn', '', time() - 3600);
     setcookie(session_name(), '', time() - 3600);
 
+    // unset and destroy session
     unset($_SESSION['loggedIn']);
     session_unset();
     session_destroy();
@@ -20,8 +21,10 @@
 <html>
     <title>Admin Page</title>
     <body>
-        <p>Hello Admin!</p>
-        <p>You logged in <?php echo htmlspecialchars($loginTime); ?></p>
+        <p>Hello!</p>
+        <?php 
+            echo "<p>You logged in {$_COOKIE['loggedIn']}</p>"
+        ?>
         <a href="login.php">Go Back</a>
     </body>
 </html>
--- lab2/login.php
+++ lab2/login.php
@@ -7,22 +7,25 @@
     session_name('login');
     session_start();
 
+    // Trigger on redirect from admin.php if user is not logged in
     if (isset($_GET['msg']) && $_GET['msg'] === 'needlogin') {
         echo "<p>You need to log in</p>";
         exit;
     }
     
+    // Checks session to see if the user is already logged in
     if (isset($_SESSION['loggedIn']) && $_SESSION['loggedIn'] === true){
         header("Location: admin.php");
         exit;
     } 
 
-    // Guard case: url parameters are set incorrectly
+    // Check if url parameters are set incorrectly
     if (!isset($_GET['user']) || !isset($_GET['password'])) {
         echo "<p>Invalid Login</p>";
         exit;
     }
 
+    // Check for credentials
     if ($_GET['user'] === $username && $_GET['password'] === $password) {
         $_SESSION['loggedIn'] = true;
         setcookie('loggedIn', date('F j, Y g:i a'), time()+60*10);
@@ -30,7 +33,8 @@
         header("Location: admin.php");
         exit;
     }
-
+    
+    // If credentials are invalid, finally alert the user that the login was invalid
     echo "<p>Invalid Login</p>";
     exit;
     
